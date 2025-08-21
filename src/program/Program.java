b5278bffde4a29ab6c34c56c5808ba05b6fb8f4a
curso <curso@ifba.irece.edu.br>
2025-08-21 09:20:12 -0300

    adicionando alterações no código

--- src/program/Program.java
+++ src/program/Program.java
@@ -2,16 +2,49 @@ package program;
 
 import db.DB;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class Program {
     
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
         
-        System.out.println("Conexao realizada com sucesso!");
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
         
-        DB.closeConnection();
+        try {
+            conn = DB.getConnection(); // abre conexão
+            System.out.println("Conexao realizada com sucesso!\n");
+            
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT * FROM seller"); // consulta a tabela
+            
+            while (rs.next()) {
+                System.out.println(
+                    rs.getInt("Id") + ", " + 
+                    rs.getString("Name") + ", " +
+                    rs.getString("Email") + ", " +
+                    rs.getDate("BirthDate") + ", " +
+                    rs.getDouble("BaseSalary") + ", " +
+                    rs.getInt("DepartmentId")
+                );
+            }
+        
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (st != null) st.close();
+                DB.closeConnection();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
+}  
     
-}
 
