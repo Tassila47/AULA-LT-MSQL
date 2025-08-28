7010b03b66b3a8d76d60a920387e18a7deec5f64
curso <curso@ifba.irece.edu.br>
2025-08-28 11:49:12 -0300

    adicionando alterções

--- src/program/Program.java
+++ src/program/Program.java
@@ -5,6 +5,9 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.Date;
+
 
 
 public class Program {
@@ -14,10 +17,27 @@ public class Program {
         Connection conn = null;
         Statement st = null;
         ResultSet rs = null;
+        PreparedStatement ps = null;
+
         
         try {
             conn = DB.getConnection(); // abre conexão
             System.out.println("Conexao realizada com sucesso!\n");
+            
+            
+            // Inserção de dados
+            String sqlInsert = "INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) VALUES (?, ?, ?, ?, ?)";
+            ps = conn.prepareStatement(sqlInsert);
+            ps.setString(1, "Fulaninho");
+            ps.setString(2, "fulaninho@gmail.com");
+            ps.setDate(3, Date.valueOf("1990-05-15"));
+            ps.setDouble(4, 2500.00);
+            ps.setInt(5, 2); 
+
+            int rowsAffected = ps.executeUpdate();
+            System.out.println("Inserção realizada! Linhas afetadas: " + rowsAffected);
+
+
             
             st = conn.createStatement();
             rs = st.executeQuery("SELECT * FROM seller"); // consulta a tabela
